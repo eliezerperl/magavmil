93959ac8a131a2932defa3258f772fe59ebf9717
Eliezer Perl <eliezerrules@gmail.com>
2024-10-16 14:22:59 +0300

    button adjusments

--- src/homePage/ButtonGrid.tsx
+++ src/homePage/ButtonGrid.tsx
@@ -1,33 +1,46 @@
 import { Button } from '@/components/ui/button';
-
+import { ChartSpline, ClipboardList, ClipboardPenLine, Info } from 'lucide-react';
 
 const ButtonGrid = () => {
-
-
   return (
     <div className="grid grid-cols-3 justify-items-center gap-4 text-black">
       <Button asChild className="button-small">
-        <a href={import.meta.env.VITE_INJURY}>דיווח על פציעה</a>
+        <a href={import.meta.env.VITE_INJURY}>
+          דיווח על פציעה
+          <ClipboardPenLine />
+        </a>
       </Button>
+
       <Button asChild className="button-small">
-        <a href={import.meta.env.VITE_SICK}>דיווח על מחלה</a>
-      </Button>
-      <Button asChild className="button-small">
-        <a href={import.meta.env.VITE_NOTIFY}>דיווח על אירוע מבצעי</a>
+        <a href={import.meta.env.VITE_NOTIFY}>
+          דיווח על אירוע מבצעי
+          <Info />
+        </a>
       </Button>
 
       <Button asChild className="button-small">
         <a href="">בקרוב</a>
       </Button>
+
+      <Button asChild className="button-small">
+        <a href={import.meta.env.VITE_SICK}>
+          דיווח על מחלה
+          <ClipboardList />
+        </a>
+      </Button>
+
       <Button asChild className="button-small">
         <a href="">בקרוב</a>
       </Button>
+
       <Button asChild className="button-small">
         <a href="">בקרוב</a>
       </Button>
 
       <Button asChild className="button-small col-span-3">
-        <a href={import.meta.env.VITE_MAGAVAPP}>אתר כח אדם ותפוקות</a>
+        <a href={import.meta.env.VITE_MAGAVAPP}>אתר כח אדם ותפוקות
+        <ChartSpline />
+        </a>
       </Button>
     </div>
   );
