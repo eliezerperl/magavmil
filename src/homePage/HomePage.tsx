82a5135cce3c5293d7a955b8e71b582d188d4133
Eliezer Perl <eliezerrules@gmail.com>
2024-10-17 21:45:45 +0300

    .

--- src/homePage/HomePage.tsx
+++ src/homePage/HomePage.tsx
@@ -6,41 +6,41 @@ const HomePage = () => {
   return (
     <>
       <div className=" h-screen w-screen relative">
-          <section className="relative flex justify-center">
-            <Button className="absolute right-6 top-4 w-20 h-16 p-4 bg-magav-dark-green text-white hover:bg-slate-600">
-              <a href={import.meta.env.VITE_MAGAVAPP}>
-                התחברות <br /> למערכת
-              </a>
-            </Button>
-            <img src="/magav.png" alt="Magav Emblem" className="w-24 h-24 " />
-          </section>
+        <section className="relative flex justify-center">
+          <Button className="absolute right-6 top-4 w-20 h-16 p-4 bg-magav-dark-green text-white hover:bg-slate-600">
+            <a href={import.meta.env.VITE_MAGAVAPP}>
+              התחברות <br /> למערכת
+            </a>
+          </Button>
+          <img src="/magav.png" alt="Magav Emblem" className="w-24 h-24 " />
+        </section>
 
-          <section className="relative flex flex-col gap-12 mb-16 justify-center font-bold">
-            <Footprints
-              className="absolute top-0 left-24 rotate-45 opacity-25"
-              color="black"
-              size={124}
-            />
-            <h1 className="text-3xl text-center z-10 text-white">
-              פורטל הדיווחים
-              <br /> של ענף המילואים <br /> משמר הגבול
-            </h1>
-            <ButtonGrid />
-          </section>
+        <section className="relative flex flex-col gap-8 mb-14 justify-center font-bold">
+          <Footprints
+            className="absolute top-0 left-24 rotate-45 opacity-25"
+            color="black"
+            size={124}
+          />
+          <h1 className="text-3xl text-center z-10 text-white">
+            פורטל הדיווחים
+            <br /> של ענף המילואים <br /> משמר הגבול
+          </h1>
+          <ButtonGrid />
+        </section>
 
-          <section className="flex justify-between text-white px-6">
-            <aside className="font-serif">
-              <span>
-                ״וּרְדַפְתֶּ֖ם אֶת־אֹיְבֵיכֶ֑ם וְנָפְל֥וּ לִפְנֵיכֶ֖ם לֶחָֽרֶב.״
-              </span>
-              <hr />
-              <div className="text-end">ויקרא כ״ו</div>
-            </aside>
-            {/* <aside className="text-center font-semibold">
+        <section className="flex justify-between text-white px-6">
+          <aside className="font-serif">
+            <span>
+              ״וּרְדַפְתֶּ֖ם אֶת־אֹיְבֵיכֶ֑ם וְנָפְל֥וּ לִפְנֵיכֶ֖ם לֶחָֽרֶב.״
+            </span>
+            <hr />
+            <div className="text-end">ויקרא כ״ו</div>
+          </aside>
+          {/* <aside className="text-center font-semibold">
               מילואים
               <br /> משמר הגבול
             </aside> */}
-          </section>
+        </section>
       </div>
     </>
   );
