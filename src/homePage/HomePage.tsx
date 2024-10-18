f45f179fdb367ac043df5717d21e4fb8fb21c0de
Eliezer Perl <eliezerrules@gmail.com>
2024-10-18 10:08:29 +0300

    .

--- src/homePage/HomePage.tsx
+++ src/homePage/HomePage.tsx
@@ -12,30 +12,32 @@ const HomePage = () => {
               התחברות <br /> למערכת
             </a>
           </Button>
-          <img src="/magav.png" alt="Magav Emblem" className="w-24 h-24 " />
+          <img src="/magav.png" alt="Magav Emblem" className="w-24 h-24 mx-6" />
         </section>
 
-        <section className="relative flex flex-col gap-8 mb-60 justify-center font-bold">
+        <section className="relative flex flex-col gap-44 mb-60 justify-center font-bold">
           <Footprints
             className="absolute top-0 left-24 rotate-45 opacity-25"
             color="black"
             size={124}
           />
-          <h1 className="text-3xl text-center z-10 text-white">
-            פורטל הדיווחים
-            <br /> של ענף המילואים <br /> משמר הגבול
-          </h1>
-          <ButtonGrid />
-        </section>
-
-        <section className="flex justify-center text-white mb-8">
-          <aside className="font-serif">
+          <aside className='flex flex-col gap-8'>
+            <h1 className="text-3xl text-center z-10 text-white">
+              פורטל הדיווחים
+              <br /> של ענף המילואים <br /> משמר הגבול
+            </h1>
+            <ButtonGrid />
+          </aside>
+          <aside className="text-center text-white font-serif">
             <span>
               ״וּרְדַפְתֶּ֖ם אֶת־אֹיְבֵיכֶ֑ם וְנָפְל֥וּ לִפְנֵיכֶ֖ם לֶחָֽרֶב.״
               {'    '}
               ויקרא כ״ו
             </span>
           </aside>
+        </section>
+
+        <section className="text-center text-white mb-8">
           {/* <aside className="text-center font-semibold">
               מילואים
               <br /> משמר הגבול
